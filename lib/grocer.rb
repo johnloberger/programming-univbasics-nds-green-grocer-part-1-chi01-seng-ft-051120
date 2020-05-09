854a40ce5847c64834c955d0ef7bef150f2a66d9
John Loberger <johnloberger@gmail.com>
2020-05-09 06:02:04 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,7 +1,5 @@
 def find_item_by_name_in_collection(name, collection)
-  collection.find do
-    |name| name == name
-  end
+  collection.find {|item| item.fetch(:name) == name}
 end
 
 def consolidate_cart(cart)
